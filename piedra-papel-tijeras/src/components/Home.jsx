d1b283a483490630a674d5aa07517faeb7dfb4ea
Agustin Di Santo <agusdisanto99@gmail.com>
2023-06-22 22:29:28 -0300

    Continuo con el Home

--- piedra-papel-tijeras/src/components/Home.jsx
+++ piedra-papel-tijeras/src/components/Home.jsx
@@ -1,19 +1,22 @@
 import "./Home.css"
-
+import SinglePlayer from "../img/SinglePlayer.png"
+import TwicePlayer from "../img/DosJugadores.png"
 
 const Home = () => {
   return (
     <div className='container-home'>
         <h1 className="container-title">¡Bienvenido!</h1>
-        <div>
+        <div className="container">
             <h2>Elige el modo de juego: </h2>
-            <div className="container-game">
-                <div>
+            <div className="container-buttons">
+                <div className="container-player">
                     <h3>Un Jugador</h3>
+                    <img src={SinglePlayer} alt="foto de singlePlayer" className="container-img"/>
                     <p>VS. Sheldon</p>
                 </div>
-                <div>
+                <div className="container-player">
                     <h3>Dos Jugadores</h3>
+                    <img src={TwicePlayer} alt="foto de singlePlayer"className="container-img" />
                     <p>Localmente</p>
                 </div>
             </div>
