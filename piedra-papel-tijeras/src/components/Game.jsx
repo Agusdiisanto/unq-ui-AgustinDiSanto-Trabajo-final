2251c51ac6183ef66321ff78c0a97aed3dc44fb0
Agustin Di Santo <agusdisanto99@gmail.com>
2023-06-26 21:28:04 -0300

    Avances en Game

--- piedra-papel-tijeras/src/components/Game.jsx
+++ piedra-papel-tijeras/src/components/Game.jsx
@@ -3,141 +3,169 @@ import { useState, useEffect } from "react"
 import { Link, useLocation } from "react-router-dom";
 import {randomOption} from "../utils/randomOption"
 import { calculateWinner } from "../utils/CalculateWinner";
+import classNames from "classnames";
 import "./Game.css"
+
 const Game = () => {
 
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const singlePlayer = queryParams.get("singlePlayer") === "true" ? true : false;
     const IAOption = singlePlayer ? randomOption() : null;
-    
-    const [firstPlayer,setFirstPlayer] = useState({
-        name:"Jugador 1",
+
+    const initialPlayerState = {
+        name: "",
         currentChoice: null,
         score: 0,
         winner: false,
-    })
+    };
 
-    const [secondPlayer,setSecondPlayer] = useState({
-        name: singlePlayer ? "IA" : "Jugador 2 ",
+    const [firstPlayer, setFirstPlayer] = useState({
+        ...initialPlayerState,
+        name: "Jugador 1",
+    });
+
+    const [secondPlayer, setSecondPlayer] = useState({
+        ...initialPlayerState,
+        name: singlePlayer ? "IA" : "Jugador 2",
         currentChoice: IAOption,
-        score: 0,
-        winner: false,
-    })
+    });
 
-    const [showModal,setShowModal]  = useState(false)
+    const [showModal, setShowModal] = useState(false);
     const [winner, setWinner] = useState(null);
     const [activePlayer, setActivePlayer] = useState(null);
-    
+
+    const checkWinner = (player) => {
+        if (!firstPlayer.currentChoice || !secondPlayer.currentChoice) {
+        return null;
+        }
+        const winner = calculateWinner(
+        firstPlayer.currentChoice,
+        secondPlayer.currentChoice
+        );
+        const isWinner = winner.nombre === player.currentChoice.nombre;
+        return isWinner ? player.score + 1 : player.score;
+    };
+
     useEffect(() => {
         if (firstPlayer.currentChoice && secondPlayer.currentChoice) {
-          setTimeout(() => {
+        setTimeout(() => {
             const winner = calculateWinner(
-              firstPlayer.currentChoice,
-              secondPlayer.currentChoice
+            firstPlayer.currentChoice,
+            secondPlayer.currentChoice
             );
             setWinner(winner);
-            if (winner.nombre === firstPlayer.currentChoice.nombre) {
-              setFirstPlayer({
-                ...firstPlayer,
-                score: firstPlayer.score + 1,
-                winner: true,
-              });
-            }
-            if (winner.nombre === secondPlayer.currentChoice.nombre) {
-              setSecondPlayer({
-                ...secondPlayer,
-                score: secondPlayer.score + 1,
-                winner: true,
-              });
-            }
-          }, 1000);
+            setFirstPlayer((prevPlayer) => ({
+            ...prevPlayer,
+            score: checkWinner(prevPlayer),
+            winner: winner.nombre === prevPlayer.currentChoice.nombre,
+            }));
+            setSecondPlayer((prevPlayer) => ({
+            ...prevPlayer,
+            score: checkWinner(prevPlayer),
+            winner: winner.nombre === prevPlayer.currentChoice.nombre,
+            }));
+        }, 1000);
         }
     }, [firstPlayer.currentChoice, secondPlayer.currentChoice]);
 
     const playAgain = (isNewGame = false) => {
         setWinner(null);
-        setFirstPlayer({
-            ...firstPlayer,
-            currentChoice: null,
-            score: isNewGame ? 0 : firstPlayer.score,
-            winner: false,
-        });
-        setSecondPlayer({
-            ...secondPlayer,
-            currentChoice: IAOption,
-            score: isNewGame ? 0 : secondPlayer.score,
-            winner: false,
-        });
-    }; 
-    const openModalSinglePlayer = () =>{
-        setActivePlayer("first")
-        setShowModal(true)
-    }
+        setFirstPlayer((prevPlayer) => ({
+        ...prevPlayer,
+        currentChoice: null,
+        score: isNewGame ? 0 : prevPlayer.score,
+        winner: false,
+        }));
+        setSecondPlayer((prevPlayer) => ({
+        ...prevPlayer,
+        currentChoice: IAOption,
+        score: isNewGame ? 0 : prevPlayer.score,
+        winner: false,
+        }));
+    };
 
-    const openModalTwoPlayer = () =>{
-        setActivePlayer("")
-        setShowModal(true)
-    }
+    const openModal = (activePlayer) => {
+        setActivePlayer(activePlayer);
+        setShowModal(true);
+    };
 
-    const buttonStyleFirst = firstPlayer.currentChoice !== null
-    ? (winner && firstPlayer.winner) ? "effect-winner button-static" : (winner && !firstPlayer.winner) ? "effect-loser button-static" : "button-static"
-    : "button-style";
+    const firstPlayerButtonClass = classNames({
+        "button-static": firstPlayer.currentChoice,
+        "button-style": !firstPlayer.currentChoice && !winner,
+        "effect-winner": winner && firstPlayer.winner,
+        "effect-loser": winner && !firstPlayer.winner,
+    });
 
-    const buttonStyleSecond = singlePlayer || secondPlayer.currentChoice !== null
-    ? (winner && secondPlayer.winner) ? "effect-winner button-static" : (winner && !secondPlayer.winner) ? "effect-loser button-static" : "button-static"
-    : "button-style";
+    const secondPlayerButtonClass = classNames({
+        "button-static": secondPlayer.currentChoice || singlePlayer,
+        "button-style": !secondPlayer.currentChoice && !singlePlayer && !winner,
+        "effect-winner": winner && secondPlayer.winner,
+        "effect-loser": winner && !secondPlayer.winner,
+    });
 
-  return (
+return (
     <div className="container-home">
-        {showModal && (
-            <ModalElemento
-                player={activePlayer === "first" ? firstPlayer : secondPlayer}
-                setPlayer={activePlayer === "first" ? setFirstPlayer : setSecondPlayer}
-                setShowModal={setShowModal}
-            />
-        )}
-        <h1 className="container-title">Game</h1>
-        <div className="container-game">
-            <div>
-                <h3 className="title-1">{firstPlayer.name}</h3>
-                <button className={`${buttonStyleFirst}`}
-                        onClick={openModalSinglePlayer}>
-                            {winner ? (firstPlayer.currentChoice.emoji) : (firstPlayer.currentChoice ? "..." : "?") }
-                </button>
-                <p className="score-text">Score : {firstPlayer.score}</p>
-            </div>
-            <div>
-                 <Link to= "/">
-                <button className="inicio-button">Inicio</button>
-                </Link>              
-            </div>
-            <div>
-                <h3 className="title-2">{secondPlayer.name}</h3>
-                <button className={`${buttonStyleSecond}`} 
-                        onClick={!singlePlayer ? openModalTwoPlayer : null}>
-                            
-                            {winner ? (secondPlayer.currentChoice.emoji) : (secondPlayer.currentChoice ? "..." : "?") }
-
-                </button>
-                <p className="score-text">Score : {secondPlayer.score}</p>
-            </div>
+    {showModal && (
+        <ModalElemento
+        player={activePlayer === "first" ? firstPlayer : secondPlayer}
+        setPlayer={activePlayer === "first" ? setFirstPlayer : setSecondPlayer}
+        setShowModal={setShowModal}
+        />
+    )}
+    <h1 className="container-title">Game</h1>
+    <div className="container-game">
+        <div>
+        <h3 className="title-1">{firstPlayer.name}</h3>
+        <button
+            className={firstPlayerButtonClass}
+            onClick={() => openModal("first")}
+        >
+            {winner
+            ? firstPlayer.currentChoice.emoji
+            : firstPlayer.currentChoice
+            ? "..."
+            : "?"}
+        </button>
+        <p className="score-text">Score: {firstPlayer.score}</p>
         </div>
-        {winner && (
-            <div className="game-container-winner">
-                <h3 className="winner-message">{winner === "empate" ? "¡Hay empate!" : `${winner.nombre} ganó!`}</h3>
-                <div className="winner-buttons">
-                    <button className="winner-button" onClick={() => playAgain(true)}>
-                        Jugar de nuevo
-                    </button>
-                    <button className="winner-button" onClick={() => playAgain()}>
-                        Siguiente
-                    </button>
-                </div>
-          </div>
-        )}
+        <div>
+        <Link to="/">
+            <button className="inicio-button">Inicio</button>
+        </Link>
+        </div>
+        <div>
+        <h3 className="title-2">{secondPlayer.name}</h3>
+        <button
+            className={secondPlayerButtonClass}
+            onClick={!singlePlayer ? () => openModal("") : null}
+        >
+            {winner
+            ? secondPlayer.currentChoice.emoji
+            : secondPlayer.currentChoice
+            ? "..."
+            : "?"}
+        </button>
+        <p className="score-text">Score: {secondPlayer.score}</p>
+        </div>
     </div>
-  )
-}
+    {winner && (
+        <div className="game-container-winner">
+        <h3 className="winner-message">
+            {winner === "empate" ? "¡Hay empate!" : `${winner.nombre} ganó!`}
+        </h3>
+        <div className="winner-buttons">
+            <button className="winner-button" onClick={() => playAgain(true)}>
+            Jugar de nuevo
+            </button>
+            <button className="winner-button" onClick={() => playAgain()}>
+            Siguiente
+            </button>
+        </div>
+        </div>
+    )}
+    </div>
+);
+};
 
-export default Game
+export default Game;
