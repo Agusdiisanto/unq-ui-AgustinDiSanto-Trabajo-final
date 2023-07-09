c77a8cd40119d7d49fac16a95cdbe145ba722b4d
Agustin Di Santo <agusdisanto99@gmail.com>
2023-07-09 15:57:13 -0300

    Aplicando responsive al video de sheldon

--- piedra-papel-tijeras/src/components/rules/SheldonVideo.jsx
+++ piedra-papel-tijeras/src/components/rules/SheldonVideo.jsx
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
+import "./SheldonVideo.css";
 
 const SheldonVideo = () => {
   return (
-   <div className="container-home">
-   <iframe width="1000" height="700" src="https://www.youtube.com/embed/S5eLybE03gc" title="YouTube video player" 
-   frameBorder="0" 
-   allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
-   gyroscope; picture-in-picture; web-share" allowfullscreen>
-   </iframe>
-    <div style={{paddingTop: "2rem"}}>
-      <Link to="/" className="text-link">Volver a inicio</Link>
+    <div className="container-video">
+      <div className="video-wrapper">
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/S5eLybE03gc"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        />
+      </div>
+      <div style={{ paddingTop: "2rem" }}>
+        <Link to="/" className="text-link">
+          Volver a inicio
+        </Link>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default SheldonVideo
+export default SheldonVideo;
